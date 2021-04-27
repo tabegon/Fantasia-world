40b52e249d3a0caddafc9afdfae7c621195da87e
tabegon <66781353+tabegon@users.noreply.github.com>
2021-04-27 13:13:06 +0200

    Ajouts des crédits

---
+++ crédits.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+import time
+
+print("\033[32midée du jeu : Léonyx\033[0m")
+time.sleep(1.5)
+print("\033[33mcorrection du jeu : tabegon\033[0m")
+time.sleep(1.5)
+print("\033[32mavancement de la version mac : Léonyx\033[0m")
+time.sleep(1.5)
+print("\033[33mavancement de la version linux : Tabegon\033[0m")
+time.sleep(1.5)
+print("\033[32mcréation des épisodes : Léonyx\033[0m")
